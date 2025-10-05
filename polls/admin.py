6502461f54a04513050b5dc1de8b9760d10b09dd
CacheCarti <mk9654@nyu.edu>
2025-10-05 11:13:46 -0400

    Update admin.py

--- polls/admin.py
+++ polls/admin.py
@@ -1,9 +1,27 @@
+# # from django.contrib import admin
+
+# # from .models import Question
+
+# # admin.site.register(Question)
+# # # Register your models here.
 # from django.contrib import admin
+# from .models import Question, Choice
 
-# from .models import Question
+# class ChoiceInline(admin.TabularInline):  # or admin.StackedInline for vertical layout
+#     model = Choice
+#     extra = 3  # number of empty choice forms shown by default
 
-# admin.site.register(Question)
-# # Register your models here.
+# class QuestionAdmin(admin.ModelAdmin):
+#     fieldsets = [
+#         (None, {'fields': ['question_text']}),
+#         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+#     ]
+#     inlines = [ChoiceInline]
+
+# admin.site.register(Question, QuestionAdmin)
+
+
+
 from django.contrib import admin
 from .models import Question, Choice
 
@@ -19,3 +37,4 @@ class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
+
